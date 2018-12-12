a18bca5bf3f66e7f44177b05cc3ebbfc490c960a
wiltrahan <twilorip@gmail.com>
2018-12-12 17:55:10 -0500

    add map/marker

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -7,12 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  latitude = 51.678418;
-  longitude = 7.809007;
+  latitude = 41.825226;
+  longitude = -71.418884;
+  locationChosen = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onChosenLocation(event) {
+    console.log(event);
+    this.latitude = event.coords.lat;
+    this.longitude = event.coords.lng;
+    this.locationChosen = true;
   }
 
 }
